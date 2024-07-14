ba3fe62df5fc0a2f46f485ea26a8e090328a8146
Roja503 <146074998+Roja503@users.noreply.github.com>
2024-07-14 18:26:52 +0530

    Create 25write_a_program_to_find_the_maximum_of_the_three_numbers_using_functions.py

---
+++ 25write_a_program_to_find_the_maximum_of_the_three_numbers_using_functions.py
@@ -0,0 +1,8 @@
+def max_num(a,b,c):
+    if a>b and a>c:
+        return "a is greater"
+    elif b>c:
+        return "b is greater"
+    else:
+        return "c is greater"
+print(max_num(10,20,30))
