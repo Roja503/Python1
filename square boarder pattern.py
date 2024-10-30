7d4708b6ac62ce0d3b2f39dde9538d36fbe3daff
Roja503 <146074998+Roja503@users.noreply.github.com>
2024-10-30 10:11:17 +0530

    Update square boarder pattern.py

--- square boarder pattern.py
+++ square boarder pattern.py
@@ -5,6 +5,6 @@ for i in range(r):
         if i==0 or i==r-1 or j==0 or j==c-1:
             print("*",end="")
         else:
-            print(" ",end="")
+            print(" ",end=" ")
         
     print()
